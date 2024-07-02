fcb88b33545b2ac71870d0f113ae8ba5372aea77
Brian <linbrian24@gmail.com>
2024-07-02 18:42:26 -0400

    Add more to models

---
+++ models/message.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  from: { type: Schema.Types.ObjectId, ref: "User" },
+  to: { type: Schema.Types.ObjectId, ref: "User" },
+  text: { type: String, required: true },
+  // ISODate ensures the date is always in an appropriate format.
+  timestamp: "ISODate('2023-01-01T00:00:00Z')",
+});
+
+User.virtual("url").get(function () {
+  return `/user/${this._id}`;
+});
+
+exports.module = mongoose.model("message", MessageSchema);
--- models/user.js
+++ models/user.js
@@ -2,17 +2,19 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const User = new Schema({
+const UserSchema = new Schema({
   username: { type: String, required: true, minLength: 3, maxLength: 20 },
   first_name: { type: String, required: true, maxLength: 20 },
   last_name: { type: String, required: true, maxLength: 20 },
   friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
-const Message = new Schema({
-  from: { type: Schema.Types.ObjectId, ref: "User" },
-  to: { type: Schema.Types.ObjectId, ref: "User" },
-  text: { type: String, required: true },
-  // ISODate ensures the date is always in an appropriate format.
-  timestamp: "ISODate('2023-01-01T00:00:00Z')",
+User.virtual("full_name").get(function () {
+  return `${first_name} ${last_name}`;
 });
+
+User.virtual("url").get(function () {
+  return `/user/${this._id}`;
+});
+
+module.exports = mongoose.model("user", UserSchema);
