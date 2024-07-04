2c4b26da11dd3866fd93885c1adf3e7b39109214
Brian <linbrian24@gmail.com>
2024-07-03 20:47:04 -0400

    Create login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,65 @@ const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-exports.user_login = (req, res) => {
-  res.send("get user");
-};
+exports.user_login = [
+  body("username", "Username must be between 3-20 characters.")
+    .trim()
+    .isLength({ min: 3, max: 20 })
+    .escape(),
+  body("password", "Password is required.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: "failure",
+        message: "invalid credentials",
+        errors: errors,
+      });
+      return;
+    }
+
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      res.status(400).json({
+        status: "failure",
+        message: "user was not found",
+      });
+      return;
+    }
+
+    const match = await bcrypt.compare(req.body.password, user.password);
+
+    if (!match) {
+      res.status(400).json({
+        status: "failure",
+        message: "invalid password",
+      });
+      return;
+    }
+
+    // Create a jwt for user authorization.
+    jwt.sign({ id: user._id }, process.env.JWT_KEY, (err, token) => {
+      if (err) {
+        res.send(err);
+        return;
+      }
+
+      res
+        .status(200)
+        .json({ status: "success", message: "user logged in", token, user });
+
+      console.log("logged in");
+    });
+  }),
+];
 
 exports.user_signup = [
   body("username", "Username must be between 3-20 characters.")
